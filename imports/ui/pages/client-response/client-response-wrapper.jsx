4955613863576ef9e0feada166df8dbd3aa0360f
Darren Angle <darrenangle@upshotmail.com>
2017-02-12 09:26:17 -0600

    more client response view shit

--- imports/ui/pages/client-response/client-response-wrapper.jsx
+++ imports/ui/pages/client-response/client-response-wrapper.jsx
@@ -8,6 +8,7 @@ import { RecapVideo } from './components/recap-video.jsx';
 import { ResponseVideoWrapper } from './components/response-video-wrapper.jsx';
 import { Downloads } from './components/downloads.jsx';
 
+import './styles/response.scss';
 
 export class ClientResponse extends Component {
 
@@ -17,12 +18,40 @@ export class ClientResponse extends Component {
 
   renderResponse(){
 
+    let clientId = this.props.params.id;
+    let client = this.props.client[0];
+    let response = this.props.response[0];
+    let recapvid = this.props.recapVideo[0] || {url: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'};
+
+    return(
+      <div className='container'>
+        <div className='row'>
+          <div className='col-xs-12 col-sm-10 col-sm-offset-1'>
+            <h1>Response for {client.name}</h1>
+            <hr/><br/>
+            <PersonalMessage
+              responseId={response._id}
+              personalMessage={response.personalMessage || 'No Personal Message Yet'}
+            />
+            <hr/><br/>
+            <RecapVideo
+              url={recapvid.url}
+            />
+          </div>
+        </div>
+        <ResponseVideoWrapper
+          videos={this.props.videos}
+          responseId={response._id}
+          clientId={clientId}
+        />
+      </div>
+    )
+
+
   }
 
   render(){
-    return(
-      <div></div>
-    )
+    return this.props.loading ? <div>Loading..</div>: this.renderResponse();
   }
 }
 
