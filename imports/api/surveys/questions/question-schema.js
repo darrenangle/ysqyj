25f75226b25d32e0f1b79f27d4f9d9069d6a5eb1
Darren Angle <darrenangle@upshotmail.com>
2017-03-15 16:06:25 -0500

    first heroku deploy

---
+++ imports/api/surveys/questions/question-schema.js
@@ -0,0 +1,50 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+export const Questions = new Mongo.Collection('questions');
+
+Questions.deny({
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; },
+})
+
+const QuestionsSchema = {};
+
+QuestionSchema.Question = new SimpleSchema({
+  _id: {
+    type:String,
+    denyUpdate: true,
+    regEx: SimpleSchema.RegEx.Id,
+    label: "The ID of the video db object"
+  },
+  createdAt: {
+    type: Date,
+    label: "Day the response video was uploaded",
+    denyUpdate: true
+  },
+  updatedAt: {
+    type: Date,
+    autoValue: function() {
+      if (this.isUpdate) {
+        return new Date();
+      }
+    },
+    denyInsert: true,
+    optional: true
+  },
+  questionText: {
+    type: String,
+    label: "the text of the question",
+    max: 2000
+  },
+  tags:{
+    type: [String]
+  },
+  "tags.$":{
+    optional: true
+    max: 200
+  }
+
+});
